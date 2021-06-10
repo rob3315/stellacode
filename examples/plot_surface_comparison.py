59796bec2a8ad555f92dd7dfb6a5e223880bc08c
Remi Robin <remi.robin@inria.fr>
2021-06-10 13:28:21 +0200

    update plot_surface

--- examples/plot_surface_comparison.py
+++ examples/plot_surface_comparison.py
@@ -1,7 +1,10 @@
-import sys,os
-sys.path.append('main/')
-from toroidal_surface import *
+import numpy as np
+import configparser
+import logging
+from src.surface.surface_Fourier import Surface_Fourier
+from src.costs.full_shape_gradient import Full_shape_gradient
 
+logging.basicConfig(level=logging.INFO)
 import pickle
 def expand_for_plot(S):
     """from a toroidal_surface surface return X,Y,Z
@@ -25,17 +28,26 @@ def plot(lst_S):
     mlab.show()
 
 if __name__=='__main__':
-    S_parametrization=Toroidal_surface.load_file('data/li383/cws.txt')
-    S=Toroidal_surface(S_parametrization,(64,64),3)
-
-    with open('output_test2','rb') as file:
+    S_parametrization=Surface_Fourier.load_file('data/li383/cws.txt')
+    path_config_file='config_file/config_full.ini'
+    config = configparser.ConfigParser()
+    config.read(path_config_file)
+    config['other']['lamb']=str(5.1e-19)
+    resolution=64
+    S=Surface_Fourier(S_parametrization,(resolution,resolution),3)
+    (m,n,Rmn,Zmn)=S_parametrization
+    full_grad=Full_shape_gradient(config=config)
+    full_grad.cost(np.concatenate((Rmn,Zmn)))
+    
+    with open('output/output_BFGS_incomplete','rb') as file:
         new_param=pickle.load(file)
     print(new_param.fun)
+    full_grad.cost(new_param.x)
     (m,n,Rmn,Zmn)=S_parametrization
     R=new_param.x[:len(m)]
     Z=new_param.x[len(m):]
-    S2=Toroidal_surface((m,n,R,Z),(64,64),3)
+    S2=Surface_Fourier((m,n,R,Z),(resolution,resolution),3)
     
-    Sp_parametrization=Toroidal_surface.load_file('data/li383/plasma_surf.txt')
-    Sp=Toroidal_surface(Sp_parametrization,(64,64),3)
+    Sp_parametrization=Surface_Fourier.load_file('data/li383/plasma_surf.txt')
+    Sp=Surface_Fourier(Sp_parametrization,(resolution,resolution),3)
     plot([S2,Sp])
