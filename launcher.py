184f692bcfa973c61092dfba5d12c03925731ed6
Remi Robin <remi.robin@inria.fr>
2021-06-11 18:04:13 +0200

    update the launcher

--- launcher.py
+++ launcher.py
@@ -1,39 +1,56 @@
 import os
 os.environ["MKL_NUM_THREADS"] = "4" # export MKL_NUM_THREADS=6
 import numpy as np
-import pickle, logging,time,sys
+import scipy.optimize
+import pickle, logging,time,sys,configparser
 from src.costs.full_shape_gradient import Full_shape_gradient
 
-path_config=sys.argv[1]
-freq_save=100
-print('path_config : {}'.format(path_config))
-full_grad=Full_shape_gradient(path_config_file=path_config)
-output_folder_name='tmp/'+full_grad.config['other']['path_output']
-os.mkdir(output_folder_name)
+def launch(path_config_file=None,config=None):
+    if config is None:
+        print('path_config : {}'.format(path_config_file))
+        config = configparser.ConfigParser()
+        config.read(path_config_file)
+    # We create the temporary folder
+    output_folder_name='tmp/'+config['other']['path_output']
+    os.mkdir(output_folder_name)
+    #We save the config file there
+    with open('{}/config.ini'.format(output_folder_name), 'w') as tmp_file:
+        config.write(tmp_file)
+    # log file
+    print('initialization of the logger')
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    filehandler = logging.FileHandler('{}/log.txt'.format(output_folder_name), 'a')
+    formatter = logging.Formatter('%(levelname)s::%(message)s')
+    filehandler.setFormatter(formatter)
+    logger.addHandler(filehandler)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(logging.WARNING)
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+    
+    full_grad=Full_shape_gradient(config=config)
+    #optimizer options
+    freq_save=int(config['optimization_parameters']['freq_save'])
+    max_iter=int(config['optimization_parameters']['max_iter'])
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-filehandler = logging.FileHandler('{}/log.txt'.format(output_folder_name), 'a')
-formatter = logging.Formatter('%(levelname)s::%(message)s')
-filehandler.setFormatter(formatter)
-logger.addHandler(filehandler)
-
-def f(X, info):
-    res = full_grad.cost(X)
-    # display information
-    if info['Nfeval']%freq_save == 0:
-        print('Neval : {0:4d} \n saving the intermediate shape'.format(info['Nfeval']))
-        with open('{}/intermediate{:}.res'.format(output_folder_name,info['Nfeval']),'wb') as output_file:
-            pickle.dump(res,output_file)
-    info['Nfeval'] += 1
-    return res
-def gradf(X, info):
-    return full_grad.shape_grad(X)
-
-#We write the config before starting
-with open('{}/config.ini'.format(output_folder_name), 'w') as tmp_file:
-    full_grad.config.write(tmp_file)
-import scipy.optimize
-optimize_shape=scipy.optimize.minimize(f, full_grad.init_param, jac=gradf,args=({'Nfeval':0},),options={'maxiter':2000,'return_all':True})
-with open('{}/result'.format(output_folder_name),'wb') as output_file:
-    pickle.dump(optimize_shape,output_file)
+    def f(X, info):
+        res = full_grad.cost(X)
+        # display information
+        if info['Nfeval']%freq_save == 0:
+            logging.warning('Neval : {0:4d} \n saving the intermediate shape'.format(info['Nfeval']))
+            with open('{}/intermediate{:}.res'.format(output_folder_name,info['Nfeval']),'wb') as output_file:
+                pickle.dump(res,output_file)
+        info['Nfeval'] += 1
+        return res
+    def gradf(X, info):
+        return full_grad.shape_grad(X)
+    #The optimization
+    optimize_shape=scipy.optimize.minimize(f, full_grad.init_param, jac=gradf,args=({'Nfeval':0},),options={'maxiter':max_iter,'return_all':True})
+    # We save the 
+    logging.warning('optimization ended, saving file')
+    with open('{}/result'.format(output_folder_name),'wb') as output_file:
+        pickle.dump(optimize_shape,output_file)
+if __name__ == '__main__':
+    #launch(sys.argv[1])# launch the script with the argument of the call python launcher.py path_to_config
+    launch('config_file/config_full.ini')
--- src/costs/aux.py
+++ src/costs/aux.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def f_non_linear(d_min_hard,d_min_soft,d_min_penalization,x):
     if x<d_min_hard:
-        logging.warning('minimal distance overeached')
+        logging.info('minimal distance overeached')
         return np.inf
         #raise Exception('minimal distance overeached')
     else :
@@ -17,14 +17,14 @@ def grad_f_non_linear(d_min_hard,d_min_soft,d_min_penalization,x):
             y=d_min_soft-x
             return d_min_penalization*(-c*y*(2*c - y))/((c - y)**2)
     else:
-        logging.warning('minimal distance overeached in gradient')
+        logging.info('minimal distance overeached in gradient')
         return -np.inf
         #raise Exception('minimal distance overeached in gradient')
 def f_e(c0,c1,x):
     if 0 <=x and x <=c1:
         return np.max((x-c0,0))**2/(1- np.max((x-c0,0))/(c1-c0))
     else:
-        logging.warning('maximal value overeached')
+        logging.info('maximal value overeached')
         return np.inf
         #raise Exception('infinite cost')
 
@@ -37,6 +37,6 @@ def grad_f_e(c0,c1,x):
             y=x-c0
             return (c*y*(2*c - y))/((c - y)**2)
     else:
-        logging.warning('maximal value overeached in gradient ')
+        logging.info('maximal value overeached in gradient ')
         return -np.inf
         #raise Exception('infinite cost in gradient')
