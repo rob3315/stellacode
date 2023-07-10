50f101d2e16510949298d5d826a0a7718eec346a
brunorigal <bruno.rigal@renfusion.eu>
2023-07-10 14:18:06 +0200

    debug vmec

--- stellacode/tools/vmec.py
+++ stellacode/tools/vmec.py
@@ -1,20 +1,128 @@
-import numpy as np
-from scipy.io import netcdf
+from stellacode import np
+import typing as tp
+from scipy.io import netcdf_file
+
+
+def _dot(coeff, cossin):
+    return np.einsum("rm,tzm->tzr", coeff, cossin)
 
 
 class VMECIO:
     def __init__(self, vmec_file: str):
-        self.file = netcdf.netcdf_file(vmec_file)
+        self.file = netcdf_file(vmec_file)
 
-    def get_val(self, label: str, theta, zeta, radius_label):
-        mnc = self.file.variables[f"{label}mnc"][()]
+    def get_val(
+        self,
+        key: str,
+        theta: np.array,
+        zeta: np.array,
+        surf_labels: tp.Optional[tp.List[int]] = None,
+        diff: tp.Optional[str] = None,
+    ):
+        xm = self.file.variables["xm_nyq"][()].astype(int)
 
-        xm = self.file.variables["xm_nyq"][()]
-        xn = self.file.variables["xm_nyq"][()]
 
-        angle = 2 * np.pi * (theta * xm + zeta * xn)
-        val = np.tensordot(mnc, np.cos(angle), 1)
-        if f"{label}mns" in self.file.variables.keys():
-            mns = self.file.variables[f"{label}mns"][()]
-            val += np.tensordot(mns, np.sin(angle), 1)
+
+        xn = self.file.variables["xn_nyq"][()].astype(int)
+        angle = theta[..., None] * xm - zeta[..., None] * xn
+
+        val = 0.0
+        if f"{key}mnc" in self.file.variables.keys():
+            mnc = self.file.variables[f"{key}mnc"][()].astype(float)
+            if surf_labels is not None:
+                mnc = mnc[surf_labels]
+            if diff == "theta":
+                val -= _dot(mnc * xm[:, None], np.sin(angle))
+            elif diff == "zeta":
+                val += _dot(mnc * xn[:, None], np.sin(angle))
+            else:
+                val += _dot(mnc, np.cos(angle))
+
+        if f"{key}mns" in self.file.variables.keys():
+            mns = self.file.variables[f"{key}mns"][()].astype(float)
+            if surf_labels is not None:
+                mns = mns[surf_labels]
+            if diff == "theta":
+                val += _dot(mns * xm[:, None], np.cos(angle))
+            elif diff == "zeta":
+                val -= _dot(mns * xn[:, None], np.cos(angle))
+            else:
+                val += _dot(mns, np.sin(angle))
+
         return val
+
+    def get_val_grad(
+        self,
+        key: str,
+        theta: np.array,
+        zeta: np.array,
+        surf_labels: tp.Optional[tp.List[int]] = None,
+    ):
+        kwargs = dict(key=key, theta=theta, zeta=zeta, surf_labels=surf_labels)
+        return np.stack(
+            (
+                self.get_val(**kwargs, diff="theta"),
+                self.get_val(**kwargs, diff="zeta"),
+            ),
+            axis=-1,
+        )
+
+    def get_b_vmec(
+        self,
+        theta: np.array,
+        zeta: np.array,
+        surf_labels: tp.Optional[tp.List[int]] = None,
+        covariant: bool = True,
+    ):
+        kwargs = dict(theta=theta, zeta=zeta, surf_labels=surf_labels)
+        if covariant:
+            prefix = "bsub"
+        else:
+            prefix = "bsup"
+        return np.stack(
+            (
+                self.get_val(f"{prefix}s", **kwargs),
+                self.get_val(f"{prefix}u", **kwargs),
+                self.get_val(f"{prefix}v", **kwargs),
+            ),
+            axis=-1,
+        )
+
+    def get_b_cylindrical(
+        self,
+        theta: np.array,
+        zeta: np.array,
+        surf_labels: tp.Optional[tp.List[int]] = None,
+    ):
+        kwargs = dict(theta=theta, zeta=zeta, surf_labels=surf_labels)
+        r_grad = self.get_val_grad("r", **kwargs)
+        z_grad = self.get_val_grad("z", **kwargs)
+        b_cov = self.get_b_vmec(**kwargs, covariant=True)
+        radius = self.get_val("r", **kwargs)
+
+        return np.stack(
+            (
+                np.einsum("tzmc,tzmc->tzm", b_cov[..., :2], r_grad),
+                radius * b_cov[..., 1],
+                np.einsum("tzmc,tzmc->tzm", b_cov[..., :2], z_grad),
+            ),
+            axis=-1,
+        )
+
+    def get_b_cartesian(
+        self,
+        theta: np.array,
+        zeta: np.array,
+        surf_labels: tp.Optional[tp.List[int]] = None,
+    ):
+        b_cyl = self.get_b_cylindrical(theta=theta, zeta=zeta, surf_labels=surf_labels)
+        zeta_ = zeta[..., None]
+
+        return np.stack(
+            (
+                b_cyl[..., 0] * np.cos(zeta_) - b_cyl[..., 1] * np.sin(zeta_),
+                b_cyl[..., 0] * np.sin(zeta_) + b_cyl[..., 1] * np.cos(zeta_),
+                b_cyl[..., -1],
+            ),
+            axis=-1,
+        )
--- test/test_vmec.py
+++ test/test_vmec.py
@@ -1,6 +1,28 @@
-from simsopt.mhd import Vmec
+import pytest
 
-# def test_vmec():
 
-# equil = Vmec("data/w7x/wout_d23p4_tm.nc")
-# import pdb;pdb.set_trace()
+@pytest.mark.skip("Missing dependency")
+def test_vmec():
+    from stellacode.tools.vmec import VMECIO
+    import utilitiesRF as urf
+    import numpy as np
+
+    vmec = VMECIO("data/w7x/wout_d23p4_tm.nc")
+    vmec2 = urf.VmecIO()
+    vmec2.read_wout("data/w7x/wout_d23p4_tm.nc")
+    vmec2.fields(nradius=200, ntheta=10, nzeta=10)
+
+    theta = np.linspace(0, 2 * np.pi, num=10)
+    zeta = np.linspace(0, 2 * np.pi, num=10)
+    zeta2D, theta2D = np.meshgrid(zeta, theta)
+
+    irad0 = np.linspace(0, 200, num=200, endpoint=True).round()
+    irad = [int(i) for i in irad0]
+
+    bvmec = vmec.get_b_vmec(zeta2D, theta2D, surf_labels=irad)
+    assert np.abs(np.transpose(bvmec[..., 0], (2, 1, 0)) - vmec2.B_s).max() < 1e-5
+    assert np.abs(np.transpose(bvmec[..., 1], (2, 1, 0)) - vmec2.B_u).max() < 1e-6
+    assert np.abs(np.transpose(bvmec[..., 2], (2, 1, 0)) - vmec2.B_v).max() < 5e-5
+
+    b_cart = vmec.get_b_cartesian(zeta2D, theta2D, surf_labels=irad)
+    b_val = vmec.get_val("b", zeta2D, theta2D)
