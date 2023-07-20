0d5c52cd588fa7e6faa25f8d7c0689b94e08cb69
brunorigal <bruno.rigal@renfusion.eu>
2023-07-20 11:24:24 +0200

    correct currents

--- stellacode/surface/current.py
+++ stellacode/surface/current.py
@@ -64,11 +64,13 @@ class Current(AbstractCurrent):
         angle = 2 * np.pi * (xm * ugrid + xn * vgrid)
 
         if self.sin_basis:
+            # Phi = sin(xm*u+xn*v)
             # dPhi/dv, -dPhi/du
             dphi.append(_stack(xn * np.cos(angle), -xm * np.cos(angle)))
 
         if self.cos_basis:
-            dphi.append(_stack(xn * np.sin(angle), -xm * np.sin(angle)))
+            # Phi = cos(xm*u+xn*v)
+            dphi.append(-_stack(xn * np.sin(angle), xm * np.sin(angle)))
 
         dphi = np.concatenate(dphi, axis=0)
         dphi = np.concatenate((np.zeros((2, lu, lv, 2)), dphi), axis=0)
@@ -83,37 +85,44 @@ class CurrentZeroTorBC(AbstractCurrent):
     num_pol: int
     num_tor: int
     sin_basis: bool = True
-    cos_basis: bool = False
+    cos_basis: bool = True
 
     def get_coeffs(self):
         grid = np.mgrid[1 : (self.num_pol + 2), 1 : (self.num_tor + 2)].reshape((2, -1))
-        return grid[0], grid[1]
+        return grid[0], grid[1], np.arange(1, (self.num_tor + 2))
 
     def get_matrix_from_grid(self, grids):
         ugrid, vgrid = grids  # u -> poloidal, v -> toroidal
         vgrid = vgrid + 0.5 / vgrid.shape[1]
         lu, lv = ugrid.shape
-        xm, xn = self.get_coeffs()
+        xm, xn, xn0 = self.get_coeffs()
         xm = xm[:, None, None]
         xn = xn[:, None, None]
+        xn0 = xn0[:, None, None]
 
         dphi = []
         assert self.sin_basis or self.cos_basis
 
         cosu = np.cos(2 * np.pi * xm * ugrid)
-        cosv = np.cos(2 * np.pi * xn * vgrid)
+        cosv = np.cos(np.pi * xn * vgrid)
         sinu = np.sin(2 * np.pi * xm * ugrid)
-        sinv = np.sin(2 * np.pi * xn * vgrid)
+        sinv = np.sin(np.pi * xn * vgrid)
         # the current potential is written as:
         # sin(2*np.pi*u)*sin(2*np.pi*v) and cos(2*np.pi*u)*sin(2*np.pi*v)
         # to ensure proper BC
         if self.sin_basis:
+            # current for Phi=sin(np.pi*xn*v)
+            cosu0 = np.cos(np.pi * xn0 * vgrid)
+            dphi.append(_stack(xn0 * cosu0, np.zeros_like(xn0 * cosu0)) / 2)
             # current for Phi=sin(2*np.pi*xm*u)*sin(2*np.pi*xn*v)
-            dphi.append(_stack(xn * sinu * cosv, -xm * cosu * sinv))
+            dphi.append(_stack(xn * sinu * cosv, -xm * cosu * sinv) / 2)
 
         if self.cos_basis:
+            # current for Phi=cos(2*np.pi*xn*v)
+            sinu0 = np.sin(np.pi * xn0 * vgrid)
+            dphi.append(_stack(-xn0 * sinu0, np.zeros_like(xn0 * sinu0)) / 2)
             # current for Phi=cos(2*np.pi*xm*u)*sin(2*np.pi*xn*v)
-            dphi.append(_stack(xn * cosu * cosv, xm * sinu * sinv))
+            dphi.append(_stack(xn * cosu * cosv, xm * sinu * sinv) / 2)
 
         dphi = np.concatenate(dphi, axis=0)
         dphi = np.concatenate((np.zeros((2, lu, lv, 2)), dphi), axis=0)
