e80af8dc675446c5ba7f26e59d94332385680e07
brunorigal <bruno.rigal@renfusion.eu>
2023-08-30 10:23:38 +0200

    reorder imports

--- stellacode/surface/__init__.py
+++ stellacode/surface/__init__.py
@@ -1,5 +1,5 @@
 from .abstract_surface import AbstractSurface, IntegrationParams
-from .current import Current, CurrentZeroTorBC, AbstractCurrent
+from .current import AbstractCurrent, Current, CurrentZeroTorBC
 from .cylindrical import CylindricalSurface
 from .fourier import FourierSurface
 from .rotated_surface import RotatedSurface
