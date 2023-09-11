eb450cddd5af939cbbd3442e5b0fc3cbba3f9ba2
Bruno Rigal <bruno.rigal@renfusion.eu>
2023-09-11 15:02:00 +0200

    solve wrong separator in windows

--- stellacode/definitions.py
+++ stellacode/definitions.py
@@ -1,5 +1,5 @@
 import typing as tp
-from os.path import dirname, realpath
+from os.path import dirname, realpath, join
 
 from pydantic import BaseModel, Extra
 
@@ -10,19 +10,19 @@ class PlasmaConfig(BaseModel):
     path_cws: tp.Optional[str] = None
 
 
-configs_folder = f"{dirname(dirname(realpath(__file__)))}/data/"
+configs_folder = join(f"{dirname(dirname(realpath(__file__)))}", "data")
 w7x_plasma = PlasmaConfig(
-    path_plasma=f"{configs_folder}w7x/wout_d23p4_tm.nc",
-    path_bnorm=f"{configs_folder}w7x/bnorm.d23p4_tm",
-    path_cws=f"{configs_folder}w7x/nescin.w7x_winding_surface_from_Drevlak",
+    path_plasma=join(configs_folder, "w7x", "wout_d23p4_tm.nc"),
+    path_bnorm=join(configs_folder, "w7x", "bnorm.d23p4_tm"),
+    path_cws=join(configs_folder, "w7x", "nescin.w7x_winding_surface_from_Drevlak"),
 )
 ncsx_plasma = PlasmaConfig(
-    path_plasma=f"{configs_folder}li383/wout_li383_1.4m.nc",
-    path_bnorm=f"{configs_folder}li383/bnorm.li383_1.4m",
-    path_cws=f"{configs_folder}li383/nescin.li383_realWindingSurface",
+    path_plasma=join(configs_folder, "li383", "wout_li383_1.4m.nc"),
+    path_bnorm=join(configs_folder, "li383", "bnorm.li383_1.4m"),
+    path_cws=join(configs_folder, "li383", "nescin.li383_realWindingSurface"),
 )
 hsx_plasma = PlasmaConfig(
-    path_plasma=f"{configs_folder}hsx/wout_HSX_QHS.nc",
+    path_plasma=join(configs_folder, "hsx", "wout_HSX_QHS.nc"),
     path_bnorm=None,
     path_cws=None,
 )
