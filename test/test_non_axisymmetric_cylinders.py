b42867410a099255fe965baca0210aa2c135c524
brunorigal <bruno.rigal@renfusion.eu>
2023-09-20 12:54:11 +0200

    debug test

--- test/test_non_axisymmetric_cylinders.py
+++ test/test_non_axisymmetric_cylinders.py
@@ -1,13 +1,8 @@
-import configparser
-
 import jax
 
 jax.config.update("jax_enable_x64", True)
-import pytest
-from scipy.io import netcdf_file
 
 from stellacode import np
-from stellacode.costs.em_cost import EMCost, get_b_field_err
 from stellacode.definitions import w7x_plasma, ncsx_plasma
 from stellacode.surface import (
     Current,
@@ -17,11 +12,7 @@ from stellacode.surface import (
 )
 from stellacode.surface import CylindricalSurface, FourierSurface
 from stellacode.surface.imports import (
-    get_current_potential,
-    get_cws,
     get_net_current,
-    get_plasma_surface,
-    get_cws_from_plasma_config,
 )
 from stellacode.surface.rotated_surface import RotatedCoil, RotatedSurface, RotateNTimes, ConcatSurfaces
 from stellacode.surface.utils import fit_to_surface
@@ -54,7 +45,7 @@ def test_non_axisymmetric_cylinders():
 
     distance = DistanceCost(
         Sp=em_cost.Sp,
-        constraint=Constraint(limit=0.2, distance=0.03, minimum=True, method=method),
+        constraint=Constraint(limit=0.2, distance=0.2, minimum=True, method=method),
     )
     current_ctr = CurrentCtrCost(constraint=Constraint(limit=100, distance=0.3, minimum=False, method=method))
     neg_curv = NegTorCurvatureCost(constraint=Constraint(limit=-0.05, distance=0.1, minimum=True, method=method))
@@ -75,11 +66,18 @@ def test_non_axisymmetric_cylinders():
             net_currents=get_net_current(w7x_plasma.path_plasma),
         )
         fourier_coeffs = np.zeros((5, 2))
+        minor_radius = em_cost.Sp.get_minor_radius()
+        major_radius = em_cost.Sp.get_major_radius()
         surface = CylindricalSurface(
             fourier_coeffs=fourier_coeffs,
             integration_par=IntegrationParams(num_points_u=num_points, num_points_v=num_points),
             num_tor_symmetry=num_sym_by_cyl,
+            radius=minor_radius * 1.5,
+            distance=major_radius,
         )
+        # surface = em_cost.Sp.get_surface_envelope(num_cyl=3, num_coeff=5, convex=True)
+        # surface.update_params(radius=surface.radius*1.5)
+
         coil_surf = CoilSurface(surface=surface, current=current)
         coil_surf = RotatedSurface(
             surface=coil_surf,
@@ -97,12 +95,11 @@ def test_non_axisymmetric_cylinders():
         method="L-BFGS-B",
         kwargs=dict(options={"disp": True, "maxls": 30, "maxiter": 5}),
     )
-    
-    cost, metrics, results, optimized_params = opt.optimize()
-    # import pdb;pdb.set_trace()
-    # coil_surf.get_distance(em_cost.Sp.xyz)
 
-    # {k:v for k,v in optimized_params.items() if "radius" in k}
+    cost, metrics, results, optimized_params = opt.optimize()
+    # j_3d = coil_surf.get_j_3D()
+    # coil_surf.surface.plot(only_one_period=True, vector_field=j_3d)
+
     metrics["deltaB_B"] = np.sqrt(metrics["cost_B"] / 1056)
-    # import pdb;pdb.set_trace()
-    assert metrics["deltaB_B"] < 0.15
+
+    assert metrics["deltaB_B"] < 0.3
