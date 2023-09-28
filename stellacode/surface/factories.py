73705f78b9ed35f44e3e8ffc21791d855cddc40c
brunorigal <bruno.rigal@renfusion.eu>
2023-09-28 12:17:53 +0200

    add free cylinder factories

--- stellacode/surface/factories.py
+++ stellacode/surface/factories.py
@@ -14,9 +14,11 @@ from stellacode.surface.tore import ToroidalSurface
 from stellacode.surface.utils import fit_to_surface
 from stellacode.tools.vmec import VMECIO
 from stellacode.surface.factory_tools import Sequential
-import numpy as np
+import jax.numpy as np
 from stellacode.surface.factory_tools import RotatedSurface, ConcatSurfaces, RotateNTimes
 from .coil_surface import CoilFactory
+from jax.typing import ArrayLike
+import jax
 
 
 def get_toroidal_surface(
@@ -154,6 +156,96 @@ def get_original_cws(path_cws: str, path_plasma: str, n_harmonics: int = 16, fac
     )
 
     return cws
+
+
+from .abstract_surface import AbstractBaseFactory
+from .coil_surface import CoilFactory
+
+
+class FreeCylinders(AbstractBaseFactory):
+    net_current: ArrayLike
+    tor_currents_w: ArrayLike
+    pol_currents_w: ArrayLike
+    coil_factory: AbstractBaseFactory
+
+    @classmethod
+    def from_plasma(
+        cls,
+        surf_plasma,
+        distance: float = 0.0,
+        num_cyl: int = 3,
+        n_harmonics: int = 8,
+        factor: int = 6,
+    ):
+        num_sym_by_cyl = surf_plasma.num_tor_symmetry * num_cyl
+        angle = 2 * np.pi / num_sym_by_cyl
+        num_points = n_harmonics * factor
+        surfaces = []
+        for n in range(num_cyl):
+            current = CurrentZeroTorBC(
+                num_pol=n_harmonics, num_tor=n_harmonics, sin_basis=True, cos_basis=True, net_currents=np.zeros(2)
+            )
+            fourier_coeffs = np.zeros((5, 2))
+            minor_radius = surf_plasma.get_minor_radius()
+            major_radius = surf_plasma.get_major_radius()
+            surface = CylindricalSurface(
+                fourier_coeffs=fourier_coeffs,
+                integration_par=IntegrationParams(num_points_u=num_points, num_points_v=num_points),
+                num_tor_symmetry=num_sym_by_cyl,
+                radius=minor_radius + distance,
+                distance=major_radius,
+            )
+
+            coil_fac_ = Sequential(
+                surface_factories=[
+                    surface,
+                    CoilFactory(current=current),
+                    RotatedSurface(
+                        rotate_n=RotateNTimes(angle=angle, max_num=n + 1, min_num=n),
+                    ),
+                ]
+            )
+            surfaces.append(coil_fac_)
+
+        coil_factory = Sequential(
+            surface_factories=[
+                ConcatSurfaces(surface_factories=surfaces),
+                RotatedSurface(
+                    rotate_n=RotateNTimes(
+                        angle=2 * np.pi / surf_plasma.num_tor_symmetry, max_num=surf_plasma.num_tor_symmetry
+                    )
+                ),
+            ]
+        )
+
+        return cls(
+            coil_factory=coil_factory,
+            net_current=get_net_current(surf_plasma.file_path),
+            tor_currents_w=np.zeros(num_cyl),
+            pol_currents_w=np.zeros(num_cyl),
+        )
+
+    def __call__(self, **kwargs):
+        tor_currents = self.net_current[0] * jax.nn.softmax(self.tor_currents_w)
+        pol_currents = self.net_current[1] * jax.nn.softmax(self.pol_currents_w)
+        coils = self.coil_factory.surface_factories[0].surface_factories
+        for i in range(len(coils)):
+            coils[i].surface_factories[1].current.net_currents = np.array([tor_currents[i], pol_currents[i]])
+
+        return self.coil_factory(**kwargs)
+
+    def get_trainable_params(self):
+        return {
+            **dict(tor_currents_w=self.tor_currents_w, pol_currents_w=self.pol_currents_w),
+            **self.coil_factory.get_trainable_params(),
+        }
+
+    def update_params(self, **kwargs):
+        for k, v in kwargs.items():
+            if k in dir(self):
+                setattr(self, k, v)
+        kwargs = {k: v for k, v in kwargs.items() if k not in ["tor_currents_w", "pol_currents_w"]}
+        self.coil_factory.update_params(**kwargs)
 
 
 def get_free_cylinders(
