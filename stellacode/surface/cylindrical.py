c7a9a6aa463bd194bd2c3d37a88477f4e193f6bd
brunorigal <bruno.rigal@renfusion.eu>
2023-10-05 16:45:59 +0200

    improve linear elasticity calculation

--- stellacode/surface/cylindrical.py
+++ stellacode/surface/cylindrical.py
@@ -18,15 +18,16 @@ class CylindricalSurface(AbstractSurfaceFactory):
         * nfp: number of field periods
         * fourier_coeffs: fourier coefficents of the cross section
         * axis_angle: rotates the surface by the given angle along the toroidal axis
-        * radius: radius of the cylinders 
+        * radius: radius of the cylinders
         * scale_length: The cylinder is scaled by the scale_length factor along the cylinder height
-        * distance: distance between the center of the cylinder and the coordinate center 
+        * distance: distance between the center of the cylinder and the coordinate center
         * make_joints: The cylinder is cut at an angle such that all rotated cylinders are joined
         * trainable_params: list of trainable parameters
     """
-    nfp: int    
+
+    nfp: int
     fourier_coeffs: tp.Optional[ArrayLike] = np.zeros((1, 2))
-    points: tp.Optional[ArrayLike] = None#np.ones(10)
+    points: tp.Optional[ArrayLike] = None  # np.ones(10)
     axis_angle: float = 0.0  # rotates the surface by the given angle
     radius: float = 1.0  # radius of the cylinders
     scale_length: float = 1.0  # The cylinder is scaled by the scale_length factor
@@ -48,7 +49,12 @@ class CylindricalSurface(AbstractSurfaceFactory):
         z_dir = np.array([0.0, 0.0, 1.0])
         assert self.fourier_coeffs is not None or self.points is not None
         if self.points is not None:
-            _radius = np.interp(u_, np.linspace(0,1, len(self.points), endpoint=False), nn.softmax(self.points)*len(self.points)) * self.radius
+            _radius = (
+                np.interp(
+                    u_, np.linspace(0, 1, len(self.points), endpoint=False), nn.softmax(self.points) * len(self.points)
+                )
+                * self.radius
+            )
         else:
             _radius = (fourier_transform(self.fourier_coeffs, u_) + 1) * self.radius
         circle = _radius * (axis_orth * np.cos(u_) + z_dir * np.sin(u_))
@@ -91,6 +97,7 @@ class CylindricalSurface(AbstractSurfaceFactory):
         ax.plot(u_, _radius, **kwargs)
         return ax
 
+
 class VerticalCylinder(AbstractSurfaceFactory):
     """
     Vertical cylindrical surface
@@ -100,6 +107,7 @@ class VerticalCylinder(AbstractSurfaceFactory):
         * height: height of the cylinder
 
     """
+
     radius: float = 1.0  # radius of the cylinder
     height: float = 1.0  # height of the cylinder
     trainable_params: tp.List[str] = [
@@ -109,6 +117,6 @@ class VerticalCylinder(AbstractSurfaceFactory):
 
     def get_xyz(self, uv):
         u_ = 2 * np.pi * uv[0]  # poloidal variable
-        v_ = uv[1] # length variable
+        v_ = uv[1] + 0.5 / self.integration_par.num_points_v  # length variable
 
-        return np.array([np.cos(u_) * self.radius, np.sin(u_)* self.radius, v_*self.height])
+        return np.array([np.cos(u_) * self.radius, np.sin(u_) * self.radius, v_ * self.height])
--- stellacode/tools/linear_elasticity.py
+++ stellacode/tools/linear_elasticity.py
@@ -1,52 +1,130 @@
-import numpy as np
+from stellacode import np
+import jax
+from jax import Array
+import typing as tp
+from pydantic import BaseModel
 
 
-def displacement_green_function(xyz_pos, xyz_force, mu: float, nu: float):
+class LinearElasticityCoeffs(BaseModel):
+    lamb: float
+    mu: float
+    nu: float
+    E: float
+
+    @classmethod
+    def from_lame(cls, lamb: float, mu: float):
+        nu = lamb / (2 * (lamb + mu))
+        E = mu * (3 * lamb + 2 * mu) / (lamb + mu)
+        return cls(lamb=lamb, mu=mu, nu=nu, E=E)
+
+    @classmethod
+    def from_E_nu(cls, E: float, nu: float):
+        lamb = E * nu / ((1 + nu) * (1 - 2 * nu))
+        mu = E / 2 * (1 + nu)
+        return cls(lamb=lamb, mu=mu, nu=nu, E=E)
+
+
+def displacement_green_function(xyz_pos: Array, xyz_force: Array, lin_coeff: LinearElasticityCoeffs) -> Array:
     """
-    \partial_j U_{i,j}
-    where U is the displacement green function
+    U_{i,k}
+    where U is the displacement green function, i is the displacement dimension and k is the force dimension
     """
     xyz = xyz_pos - xyz_force
     dist = np.linalg.norm(xyz)
-    G = 1 / (16 * (1 - nu) * np.pi * mu * dist) * ((3 - 4 * nu) * np.eye(3) + xyz[:, None] * xyz[None] / dist**2)
+    G = (
+        1
+        / (16 * (1 - lin_coeff.nu) * np.pi * lin_coeff.mu * dist)
+        * ((3 - 4 * lin_coeff.nu) * np.eye(3) + xyz[:, None] * xyz[None] / dist**2)
+    )
     return G
 
 
-def displacement_green_function(xyz_pos, xyz_force, mu: float, lamb: float):
-    """
-    \partial_j U_{i,j}
-    where G is the stress green function
-    """
-    xyz = xyz_pos - xyz_force
-    dist = np.linalg.norm(xyz)
-    nu = get_nu(lamb, mu)
-    C = 1 / (16 * (1 - nu) * np.pi * mu)
+# def grad_displacement_green_function(xyz_pos, xyz_force, mu: float, nu: float):
+#     """
+#     \partial_k U_{i,j}
+#     where U is the displacement green function
+#     """
+#     xyz = xyz_pos - xyz_force
+#     dist = np.linalg.norm(xyz)
+#     # nu = get_nu(lamb, mu)
+#     C = 1 / (16 * (1 - nu) * np.pi * mu)
 
-    xk = xyz[:, None, None]
-    xi = xyz[None, :, None]
-    xj = xyz[None, None, :]
-    return -xk / dist**2 * displacement_green_function(xyz_pos, xyz_force, mu, nu) + C * (
-        (np.diag(3)[:, :, None] * xj + np.diag(3)[:, None, :] * xi) / dist**3 - 4 * xi * xj * xk / dist**4
+#     xk = xyz[:, None, None]
+#     xi = xyz[None, :, None]
+#     xj = xyz[None, None, :]
+#     return -xk / dist**2 * displacement_green_function(xyz_pos, xyz_force, mu, nu)[None, :, :] + C * (
+#         (np.eye(3)[:, :,None] * xj + np.eye(3)[:,None, :] * xi) / dist**3 - 4 * xi * xj * xk / dist**4
+#     )
+
+# The jacobian adds a dimension along the last dimension
+grad_displacement_green_function = jax.jacobian(displacement_green_function)
+
+
+def strain_green_function(xyz_pos: Array, xyz_force: Array, lin_coeff: LinearElasticityCoeffs) -> Array:
+    """
+    strain green function \Epsilon_ikj where i and j are the strain dimensions and k is the force dimension
+    """
+    du = grad_displacement_green_function(xyz_pos, xyz_force, lin_coeff)
+    return (du + np.transpose(du, (2, 1, 0))) / 2
+
+
+def stress_green_function(xyz_pos: Array, xyz_force: Array, lin_coeff: LinearElasticityCoeffs) -> Array:
+    """
+    stress green function \Sigma_ikj where i and j are the stress dimensions and k is the force dimension
+    """
+
+    epsilon_gf = strain_green_function(xyz_pos, xyz_force, lin_coeff)
+
+    return (
+        lin_coeff.lamb * np.trace(epsilon_gf, axis1=0, axis2=2) * np.eye(3)[:, None, :] + 2 * lin_coeff.mu * epsilon_gf
     )
 
 
-def stress_green_function(xyz_pos, xyz_force, mu: float, lamb: float):
-    du = displacement_green_function(xyz_pos, xyz_force, mu, lamb)
-    epsilon = du + np.transpose(du, (1, 0, 2))
+def integral_gf_single_point(
+    xyz_req: Array, xyz_int: Array, fun: tp.Callable, ds_int: Array, vector_field: Array
+) -> Array:
+    vals = jax.vmap(fun, in_axes=(None, 0))(xyz_req, xyz_int)
 
-    sigma = lamb * np.trace(epsilon, axis1=0, axis2=1) * np.eye(3)[:, :, None] + 2 * mu * epsilon
-    return sigma
-
-
-def to_lame(E, nu):
-    lamb = E * nu / ((1 + nu) * (1 - 2 * nu))
-    mu = E / 2 * (1 + nu)
-    return lamb, mu
+    return np.einsum("dikj,dk,d->ij", vals, vector_field, ds_int)
 
 
-def get_nu(lamb, mu):
-    return lamb / (2 * (lamb + mu))
+integral_gf = jax.vmap(integral_gf_single_point, in_axes=(0, None, None, None, None))
 
 
-def get_E(lamb, mu):
-    return mu * (3 * lamb + 2 * mu) / (lamb + mu)
+def integral_gf1_single_point(
+    xyz_req: Array, xyz_int: Array, fun: tp.Callable, ds_int: Array, vector_field: Array
+) -> Array:
+    vals = jax.vmap(fun, in_axes=(None, 0))(xyz_req, xyz_int)
+
+    return np.einsum("dik,dk,d->i", vals, vector_field, ds_int)
+
+
+integral_gf1 = jax.vmap(integral_gf1_single_point, in_axes=(0, None, None, None, None))
+
+
+def get_stress_from_force(
+    surf_coil, xyz_req: Array, force: Array, lin_coeff: LinearElasticityCoeffs, nfp: int
+) -> tp.Tuple[Array, Array]:
+    lu, lv, _ = xyz_req.shape
+    stress = integral_gf(
+        np.reshape(xyz_req, (-1, 3)),
+        np.reshape(surf_coil.xyz, (-1, 3)),
+        lambda x, y: stress_green_function(x, y, lin_coeff=lin_coeff),
+        np.reshape(surf_coil.ds, -1) * surf_coil.dudv,
+        np.reshape(force, (-1, 3)),
+    )
+    return np.reshape(stress, (lu, lv, 3, 3)), force
+
+
+def get_displacement_from_force(
+    surf_coil, xyz_req: Array, force: Array, lin_coeff: LinearElasticityCoeffs, nfp: int
+) -> tp.Tuple[Array, Array]:
+    lu, lv, _ = xyz_req.shape
+    displacement = integral_gf1(
+        np.reshape(xyz_req, (-1, 3)),
+        np.reshape(surf_coil.xyz, (-1, 3)),
+        lambda x, y: displacement_green_function(x, y, lin_coeff=lin_coeff),
+        np.reshape(surf_coil.ds, -1) * surf_coil.dudv,
+        np.reshape(force, (-1, 3)),
+    )
+    return np.reshape(displacement, (lu, lv, 3))
